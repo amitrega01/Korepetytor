816e0087dbc91787e235a08d40bfb20990226049
Adam <amitrega01@gmail.com>
2019-04-09 11:45:49 +0200

    added new screens

--- App.js
+++ App.js
@@ -3,10 +3,16 @@ import { View } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import FirstRunScreen from './src/screens/FirstRunScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
+import SignInScreen from './src/screens/SignInScreen';
+import StudentHomeScreen from './src/screens/StudentHomeScreen';
+import CoachHomeScreen from './src/screens/CoachHomeScreen';
 
 const MainNavigator = createStackNavigator({
   FirstRun: { screen: FirstRunScreen },
-  SignUp: { screen: SignUpScreen }
+  SignUp: { screen: SignUpScreen },
+  SignIn: { screen: SignInScreen },
+  StudentHome: { screen: StudentHomeScreen },
+  CoachHome: { screen: CoachHomeScreen }
 });
 
 const AppContainer = createAppContainer(MainNavigator);
