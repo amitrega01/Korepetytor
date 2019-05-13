88c00823678cea5bdebb56e42c36de2efeb6ec50
Adam <amitrega01@gmail.com>
2019-05-13 17:51:09 +0200

    new screen

--- App.js
+++ App.js
@@ -6,13 +6,15 @@ import SignUpScreen from './src/screens/SignUpScreen';
 import SignInScreen from './src/screens/SignInScreen';
 import StudentHomeScreen from './src/screens/StudentHomeScreen';
 import CoachHomeScreen from './src/screens/CoachHomeScreen';
+import CoachProfileScreen from './src/screens/CoachProfileScreen';
 
 const MainNavigator = createStackNavigator({
   FirstRun: { screen: FirstRunScreen },
   SignUp: { screen: SignUpScreen },
   SignIn: { screen: SignInScreen },
   StudentHome: { screen: StudentHomeScreen },
-  CoachHome: { screen: CoachHomeScreen }
+  CoachHome: { screen: CoachHomeScreen },
+  CoachProfile: { screen: CoachProfileScreen }
 });
 
 const AppContainer = createAppContainer(MainNavigator);
